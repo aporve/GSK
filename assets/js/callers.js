16570e2f88d41db3b992143f5609cca281ea9278
aporve <aporvep@gmail.com>
2022-11-02 15:49:13 +0530

    Update the code for event

--- assets/js/callers.js
+++ assets/js/callers.js
@@ -302,5 +302,16 @@ window.addEventListener('message', function (eventData) {
         ToApp(eventName,data);
     }
 
+    if (parsedEventData.event_code === "ordercart-screen" && parsedEventData.data) {
+        // document.querySelector("iframe").contentWindow.postMessage(JSON.stringify({
+        //     event_code: 'ordercart-screen',                                                // added new event name
+        //     data: parsedEventData.data
+        // }), '*');
+        let eventName = parsedEventData.event_code;
+        let data = parsedEventData.data;
+        console.log("eventName---", eventName);
+        console.log('Event Data---',data);
+        ToApp(eventName,data);
+    }
 
 });
