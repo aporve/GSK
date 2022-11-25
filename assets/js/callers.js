687f50152d64ae50fd92557dbf2141b9bf5e8993
aporve <aporvep@gmail.com>
2022-11-25 14:44:02 +0530

    Update the code

--- assets/js/callers.js
+++ assets/js/callers.js
@@ -386,8 +386,8 @@ window.addEventListener('message', function (eventData) {
         let data = parsedEventData.data;
         console.log("eventName---", eventName);
         console.log('refreshed local storage data in caller.js',data);
-        console.log('reload parse data',JSON.parse(data))
-        ToApp("get-data-on-refresh", JSON.parse(data));
+        console.log('reload parse data',data)
+        ToApp("get-data-on-refresh", data);
     }
 
 });
