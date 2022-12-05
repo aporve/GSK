3b36170baa8ca476c8de62268e445370ed2f2dd9
aporve <aporvep@gmail.com>
2022-12-05 18:50:40 +0530

    Added the code

--- assets/js/parentIframe.js
+++ assets/js/parentIframe.js
@@ -17,7 +17,7 @@ function injectDynamicCssToChild() {
     var ymFrameHead = window.frames["ymIframe"].document.getElementsByTagName("head")[0];
     var modularStyles = document.createElement('style');
     modularStyles.type = 'text/css';
-   var css = '#chatBoxMainContainer.message-icons.live-chat { margin: 3.5rem 0 0 40%; background-image: url("https://cdn.yellowmessenger.com/zlqqXMmsw3z91663746912397.png")}*{margin:0;padding:0}.mod-head-side-bar-container .overflow-container-sidebar{position:absolute;top:0;left:0;right:0;bottom:0;}.mod-head-side-bar-container .overflow-container-sidebar .sidebar-wrapper{display:flex;flex-direction:column;justify-content:center;background-color:#fff;padding:0;width:40%;list-style:none;height:100%;justify-content:space-between;border:.5px solid #ccc}.mod-head-side-bar-container .overflow-container-sidebar .sidebar-wrapper .side-bar-icon-box{text-decoration:none;color:#fff;padding:5px;cursor:pointer;display:flex}.mod-head-side-bar-container .overflow-container-sidebar .sidebar-wrapper .side-bar-icon-box:hover{background:#caf7e3;border-right:3px solid #39a6a3}.mod-head-side-bar-container .overflow-container-sidebar .sidebar-wrapper .side-bar-icon-box .sidebar-img-icon{width:3.5rem;height:auto;pointer-events:none;}.mod-head-side-bar-container .overflow-container-header{position:fixed;top:0;right:0;width:calc(100% - 40% - 2px);z-index:99999}.mod-head-side-bar-container .overflow-container-header .header-wrapper{display:flex;flex-direction:row;align-items:center;justify-content:space-between;background-color:#fff;padding:0;list-style:none;height:3.5rem;border:.5px solid #ccc;border-left:0;}.mod-head-side-bar-container .overflow-container-header .header-wrapper .header-title{font-size:16px;font-weight:600;color:#999;padding:0 1rem}.mod-head-side-bar-container .overflow-container-header .header-wrapper .icons-box{padding:0 1rem}.mod-head-side-bar-container .overflow-container-header .header-wrapper .icons-box .header-bar-icon-box{color:#000;font-weight:900;font-size:20px;width:100%;padding-left:.5rem;cursor:pointer}.mod-head-side-bar-container .overflow-container-header .header-wrapper .icons-box .header-bar-icon-box .header-icons{width:1.2rem;height:auto} .message-icons.live-chat .noPaddingLeft.cardsNoBotPersona { width:100%}  @media only screen and (min-width: 768px) {.cards.slick-initialized .slick-slide {height: auto;border-radius: 10px;position: relative;margin-bottom: 10px;overflow: hidden;/* width: 300px; */max-width: none; min-width: none; display: inline-block !important;width: 700px;padding-bottom: 0px !important;margin-right: 10px !important;box-shadow: rgb(0 0 0 / 10%) 0px 1px 3px 0px, rgb(0 0 0 / 10%) 0px 0px 1px 0px !important;} }  .send-input { margin-left:40%} #chatDetails { margin-left:40%} .blur{ -webkit-filter: blur(30px);} #chatBoxMain { padding: 0 0 31px 0; } #chatOptions { padding: 0 0 31px 0; }';
+    var css = '#chatBoxMainContainer.message-icons.live-chat { margin: 3.5rem 0 0 40%; background-image: url("https://cdn.yellowmessenger.com/zlqqXMmsw3z91663746912397.png")}*{margin:0;padding:0}.mod-head-side-bar-container .overflow-container-sidebar{position:absolute;top:0;left:0;right:0;bottom:0;}.mod-head-side-bar-container .overflow-container-sidebar .sidebar-wrapper{display:flex;flex-direction:column;justify-content:center;background-color:#fff;padding:0;width:40%;list-style:none;height:100%;justify-content:space-between;border:.5px solid #ccc}.mod-head-side-bar-container .overflow-container-sidebar .sidebar-wrapper .side-bar-icon-box{text-decoration:none;color:#fff;padding:5px;cursor:pointer;display:flex}.mod-head-side-bar-container .overflow-container-sidebar .sidebar-wrapper .side-bar-icon-box:hover{background:#caf7e3;border-right:3px solid #39a6a3}.mod-head-side-bar-container .overflow-container-sidebar .sidebar-wrapper .side-bar-icon-box .sidebar-img-icon{width:3.5rem;height:auto;pointer-events:none;}.mod-head-side-bar-container .overflow-container-header{position:fixed;top:0;right:0;width:calc(100% - 40% - 2px);z-index:99999}.mod-head-side-bar-container .overflow-container-header .header-wrapper{display:flex;flex-direction:row;align-items:center;justify-content:space-between;background-color:#fff;padding:0;list-style:none;height:3.5rem;border:.5px solid #ccc;border-left:0;}.mod-head-side-bar-container .overflow-container-header .header-wrapper .header-title{font-size:16px;font-weight:600;color:#999;padding:0 1rem}.mod-head-side-bar-container .overflow-container-header .header-wrapper .icons-box{padding:0 1rem}.mod-head-side-bar-container .overflow-container-header .header-wrapper .icons-box .header-bar-icon-box{color:#000;font-weight:900;font-size:20px;width:100%;padding-left:.5rem;cursor:pointer}.mod-head-side-bar-container .overflow-container-header .header-wrapper .icons-box .header-bar-icon-box .header-icons{width:1.2rem;height:auto} .message-icons.live-chat .noPaddingLeft.cardsNoBotPersona { width:100%}  @media only screen and (min-width: 768px) {.cards.slick-initialized .slick-slide {height: auto;border-radius: 10px;position: relative;margin-bottom: 10px;overflow: hidden;/* width: 300px; */max-width: none; min-width: none; display: inline-block !important;width: 700px;padding-bottom: 0px !important;margin-right: 10px !important;box-shadow: rgb(0 0 0 / 10%) 0px 1px 3px 0px, rgb(0 0 0 / 10%) 0px 0px 1px 0px !important;} }  .send-input { margin-left:40%} #chatDetails { margin-left:40%} .blur{ -webkit-filter: blur(30px);} #chatBoxMain { padding: 0 0 31px 0; } #chatOptions { padding: 0 0 31px 0; }';
     if (modularStyles.styleSheet) {
         modularStyles.styleSheet.cssText = css;
     } else {
@@ -55,8 +55,8 @@ window.addEventListener('message', function (eventData) {
     let parsedData = JSON.parse(eventData.data);
 
     console.log("parsedData", parsedData)
-    
-   
+
+
 
     if (parsedData?.event_code == 'custom-event' && parsedData?.data?.code == "welcome-screen") {
         document.getElementById('ymIframe').contentWindow.postMessage(JSON.stringify({
@@ -64,7 +64,7 @@ window.addEventListener('message', function (eventData) {
             data: parsedData.data.data
         }), '*');
         console.log("welcome-screen")
-        console.log('Event Data-->>',parsedData.data.data)
+        console.log('Event Data-->>', parsedData.data.data)
         return;
     }
 
@@ -74,7 +74,7 @@ window.addEventListener('message', function (eventData) {
             data: parsedData.data.data
         }), '*');
         console.log("termsui-screen")
-        console.log('Event Data-->>',parsedData.data.data)
+        console.log('Event Data-->>', parsedData.data.data)
         return;
     }
 
@@ -83,59 +83,59 @@ window.addEventListener('message', function (eventData) {
             event_code: 'userwelcome-screen',
             data: parsedData.data.data
         }), '*');
-        
+
         console.log("userwelcome-screen")
-        console.log('Event Data-->>',parsedData.data.data)
+        console.log('Event Data-->>', parsedData.data.data)
         return;
     }
-    
+
     if (parsedData?.event_code == 'custom-event' && parsedData?.data?.code == "user-login") {
         document.getElementById('ymIframe').contentWindow.postMessage(JSON.stringify({
             event_code: 'user-login',
-            data:  parsedData.data.data
+            data: parsedData.data.data
         }), '*');
         console.log("user-login")
-        console.log('Event Data-->>',parsedData.data.data)
+        console.log('Event Data-->>', parsedData.data.data)
         return;
     }
 
     if (parsedData?.event_code == 'custom-event' && parsedData?.data?.code == "ordercart-screen") {
         document.getElementById('ymIframe').contentWindow.postMessage(JSON.stringify({
             event_code: 'ordercart-screen',
-            data:  parsedData.data.data
+            data: parsedData.data.data
         }), '*');
         console.log("ordercart-screen")
-        console.log('Event Data-->>',parsedData.data.data)
+        console.log('Event Data-->>', parsedData.data.data)
         return;
     }
 
     if (parsedData?.event_code == 'custom-event' && parsedData?.data?.code == "load-userwelcome-screen") {
         document.getElementById('ymIframe').contentWindow.postMessage(JSON.stringify({
             event_code: 'load-userwelcome-screen',
-            data:  parsedData.data.data
+            data: parsedData.data.data
         }), '*');
         console.log("load-userwelcome-screen")
-        console.log('Event Data-->>',parsedData.data.data)
+        console.log('Event Data-->>', parsedData.data.data)
         return;
     }
 
     if (parsedData?.event_code == 'custom-event' && parsedData?.data?.code == "show-brand-selection") {
         document.getElementById('ymIframe').contentWindow.postMessage(JSON.stringify({
             event_code: 'show-brand-selection',
-            data:  parsedData.data.data
+            data: parsedData.data.data
         }), '*');
         console.log("show-brand-selection")
-        console.log('Event Data-->>',parsedData.data.data)
+        console.log('Event Data-->>', parsedData.data.data)
         return;
     }
 
     if (parsedData?.event_code == 'custom-event' && parsedData?.data?.code == "show-brand-detailing") {
         document.getElementById('ymIframe').contentWindow.postMessage(JSON.stringify({
             event_code: 'show-brand-detailing',
-            data:  parsedData.data.data
+            data: parsedData.data.data
         }), '*');
         console.log("show-brand-detailing")
-        console.log('Event Data-->>',parsedData.data.data)
+        console.log('Event Data-->>', parsedData.data.data)
         return;
     }
 
@@ -143,10 +143,10 @@ window.addEventListener('message', function (eventData) {
         console.log("bot-reloaded");
         let data = localStorage.getItem("updated-data")
         console.log('refreshed local storage data in parentIframe', data);
-        if(!data) return;
+        if (!data) return;
         document.getElementById('ymIframe').contentWindow.postMessage(JSON.stringify({
             event_code: 'bot-reloaded',
-            data:  data
+            data: data
         }), '*');
         return;
     }
@@ -156,8 +156,8 @@ window.addEventListener('message', function (eventData) {
     // Send events to bot
 
     if (parsedData?.event_code == 'logout') {
-       console.error('-logout--')
-       localStorage.removeItem("updated-data");
+        console.error('-logout--')
+        localStorage.removeItem("updated-data");
         console.log("\n\n\n <--- Logout event in parent iframe ---> \n\n\n", parsedData);
         window.frames.ymIframe.chat.send({
             event: {
@@ -291,7 +291,13 @@ window.addEventListener('message', function (eventData) {
 
     if (parsedData?.event_code == 'update-data-on-refresh') {
         console.log("\n\n\n <--- update-data-on-refresh event in parent iframe ---> \n\n\n", parsedData);
-        localStorage.setItem("updated-data", parsedData.data)
+        localStorage.setItem("updated-data", parsedData.data);
+        window.frames.ymIframe.chat.send({
+            event: {
+                code: "update-data-on-refresh",
+                data: 'data'
+            }
+        }, true);
         return;
     }
 
